e22b3e2c5e196d555e3961345808b24dfe8ab3c5
Ian Zunderdorp <ian.zunderdorp@mobiliteitsfabriek.nl>
2020-10-22 11:27:56 +0200

    Automatic PR reviewer for ISAE compliance

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
+const config = {
+    pullRequestTitleRegex: /^\[?(mofab|io)-\d+\]?/i,
+    branchNameRegex: /^[^\/]+\/(mofab|io)-\d+-/i
+};
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    try {
+        const {repository, pull_request} = github.context.payload;
+        const octokitPullsPayload = {
+            owner: repository.owner.login,
+            repo: repository.name,
+            pull_number: pull_request.number
+        };
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+        const octokit = github.getOctokit(core.getInput('repo-token', {required: true}));
+        const errors = [];
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        if (false === config.pullRequestTitleRegex.test(pull_request.title)) {
+            errors.push('* Title does not seem to contain reference to JIRA ticket');
+        }
+
+        if (false === config.branchNameRegex.test(pull_request.head.ref)) {
+            errors.push('* Branch name does not seem to contain reference to JIRA ticket (expected ^[^/]+/(MOFAB|IO)-\d+-.*$)');
+        }
+
+        const reviews = octokit.pulls.listReviews(octokitPullsPayload);
+        const reviewExists = reviews.data.any(review => review.user.login === 'github-actions[bot]');
+
+        if (errors.length > 0) {
+            if (false === reviewExists) {
+                octokit.pulls.createReview({
+                    ...octokitPullsPayload,
+                    body: errors.join('\n'),
+                    event: 'REQUEST_CHANGES'
+                });
+            } else {
+                octokit.pulls.createReview({
+                    ...octokitPullsPayload,
+                    body: errors.join('\n'),
+                    event: 'COMMENT'
+                });
+            }
+        } else {
+            if (reviewExists) {
+                for (const review of reviews.data) {
+                    octokit.pulls.dismissReview({
+                        ...octokitPullsPayload,
+                        review_id: review.id,
+                        message: 'PR title & branch is now ISAE compliant!'
+                    });
+                }
+            }
+        }
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
